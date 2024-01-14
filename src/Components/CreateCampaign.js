3f6d3c9c58d7b981bb942ab41764c94fa5d32994
Omar Mohsin <Omarabdessalam4748@gmail.com>
2024-01-14 12:58:23 +0300

    adding react persist

--- src/Components/CreateCampaign.js
+++ src/Components/CreateCampaign.js
@@ -6,7 +6,9 @@ import { useNavigate } from "react-router";
 import Autocomplete from "@mui/material/Autocomplete";
 import Chip from "@mui/material/Chip";
 import TextField from "@mui/material/TextField";
+import { Checkbox } from "@mui/material";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function CreateCampaign() {
   const user = useSelector(SelectUser);
@@ -21,20 +23,24 @@ function CreateCampaign() {
     start_data: "",
     end_data: "",
   });
+  const top100Films = [];
 
   const [error, setError] = useState();
   const [message, setMessage] = useState("");
   const onChange = (event) => {
     setData({ ...data, [event.target.name]: event.target.value });
   };
-  const goToLogin = () => {
-    navigate("/login");
-  };
+  function toggleCheckbox(arr, value) {
+    if (arr.includes(value)) {
+      return arr.filter((item) => item !== value);
+    } else {
+      return [...arr, value];
+    }
+  }
+
   const goToCampaign = () => {
     navigate("/campaign");
   };
-
-  const top100Films = [];
 
   const onChangeValues = (event, newValue) => {
     console.log(newValue);
@@ -91,7 +97,8 @@ function CreateCampaign() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-4 md:p-8 rounded-lg md:w-2/3 lg:w-1/2 xl:w-1/3 text-gray-800 flex flex-col items-center space-y-4">
+      <div className="bg-white p-4 md:p-8 rounded-lg md:w-2/3 lg:w-1/2 xl:w-1/3 text-gray-800 flex flex-col items-center space-y-4 overflow-auto">
+        {" "}
         {user ? (
           <div className="w-full max-w-md">
             {error && (
@@ -100,7 +107,7 @@ function CreateCampaign() {
               </p>
             )}
             <div className="mb-10 flex justify-center text-2xl">
-              create campaign
+              Create Campaign
             </div>
 
             <InputField
@@ -115,6 +122,7 @@ function CreateCampaign() {
               multiple
               id="tags-filled"
               freeSolo
+              className="mb-5"
               options={top100Films?.map((option) => option.title)}
               value={values}
               onChange={onChangeValues}
@@ -136,6 +144,87 @@ function CreateCampaign() {
                 />
               )}
             />
+            <div className="mb-4">
+              <p className="font-semibold mb-2">Platform:</p>
+              <div className="flex flex-wrap gap-4">
+                <div>
+                  <label>
+                    Facebook
+                    <Checkbox
+                      size="small"
+                      checked={data.platform.includes("Facebook")}
+                      onChange={() =>
+                        setData({
+                          ...data,
+                          platform: toggleCheckbox(data.platform, "Facebook"),
+                        })
+                      }
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    LinkedIn
+                    <Checkbox
+                      size="small"
+                      checked={data.platform.includes("LinkedIn")}
+                      onChange={() =>
+                        setData({
+                          ...data,
+                          platform: toggleCheckbox(data.platform, "LinkedIn"),
+                        })
+                      }
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Telegram
+                    <Checkbox
+                      size="small"
+                      checked={data.platform.includes("Telegram")}
+                      onChange={() =>
+                        setData({
+                          ...data,
+                          platform: toggleCheckbox(data.platform, "Telegram"),
+                        })
+                      }
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Instagram
+                    <Checkbox
+                      size="small"
+                      checked={data.platform.includes("Instagram")}
+                      onChange={() =>
+                        setData({
+                          ...data,
+                          platform: toggleCheckbox(data.platform, "Instagram"),
+                        })
+                      }
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Youtube
+                    <Checkbox
+                      size="small"
+                      checked={data.platform.includes("Youtube")}
+                      onChange={() =>
+                        setData({
+                          ...data,
+                          platform: toggleCheckbox(data.platform, "Youtube"),
+                        })
+                      }
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+
             <InputField
               type="datetime-local"
               placeholder="Start Date"
@@ -152,6 +241,7 @@ function CreateCampaign() {
               onChange={onChange}
               className="mb-4"
             />
+            <div className="flex justify-center flex-col">
             <button
               onClick={onSubmitHandler}
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 md:py-3 px-4 md:px-6 rounded-full transition duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -160,10 +250,11 @@ function CreateCampaign() {
             </button>
             <button
               onClick={goToCampaign}
-              className="bg-green-500 hover:bg-green-600 mt-4 text-white font-bold py-2 md:py-3 px-4 md:px-6 rounded-full transition duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="bg-green-500 hover:bg-green-600 mt-4 text-white font-bold py-2 md:py-3 px-4 md:px-6 rounded-full transition duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 "
             >
               Back to Campaign Page
             </button>
+            </div>
             {message && (
               <p className="text-green-500 mt-4 bg-green-100 border border-green-400 rounded p-2">
                 {message}
@@ -172,9 +263,11 @@ function CreateCampaign() {
           </div>
         ) : (
           <>
-            <h1 className="text-4xl mb-6 text-red-600 font-semibold">
-              Please Login
-            </h1>
+            <Link to={"/login"}>
+              <h1 className="text-4xl mb-6 text-red-600 font-semibold">
+                Please Login
+              </h1>
+            </Link>
           </>
         )}
       </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./auth/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import authReducer from './auth/authSlice';
+const rootReducers = combineReducers({
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducers);
 
 export const store = configureStore({
-    reducer: {
-        auth  : authReducer,
-    }
-})
+  reducer: persistedReducer,
+});
+export const persistor = persistStore(store);
